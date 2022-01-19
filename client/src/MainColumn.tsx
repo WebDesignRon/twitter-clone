b1636e73a88ad61d862fb8a8f478508b39689e16
okathira <43328895+okathira@users.noreply.github.com>
2022-01-19 10:35:06 +0900

    デバッグ用表示を消す

--- client/src/MainColumn.tsx
+++ client/src/MainColumn.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import TweetScroller from './TweetScroller';
-import { UserInfoInput } from './components/molecules/input';
 
 const MainColumn: React.FC = () => (
   <div className="m-0 h-full min-h-screen w-150 border-x">
     <div className="sticky top-0 px-2.5 py-3 border-b bg-white">
       <span className="text-black font-bold text-xl">ホーム</span>
     </div>
-    <UserInfoInput name="username" maxLength={30} labelName="名前" />
     <TweetScroller />
   </div>
 );
